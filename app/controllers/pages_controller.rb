6227bbeb7d705d78b43e5b477285f56bc0c2752b
thebudjr <tim.s.janney@gmail.com>
2015-08-30 17:42:35 -0400

    added dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
+	before_action :authenticate_user!, only: [:dashboard]
+
   def home
   	if current_user
   		redirect_to pictures_path
   	end
+  end
+
+  def dashboard
+  	@pictures = Picture.all
   end
 end
